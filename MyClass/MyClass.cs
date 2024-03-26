0ed39e13527123829dd9a76afe31bb93b5cc7beb
LizethCarriQui <LizethCarriQui@LIIZETHQUICARRI>
2024-03-26 13:16:51 -0600

    Agregar archivos de proyecto.

---
+++ MyClass/MyClass.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace MyClass
+{
+    public class MyClass
+    {
+        private int miInteger;
+        private string myString;
+
+        public MyClass()
+        {
+            miInteger = 0;
+            myString = "Worthless";
+        }
+
+       // Parameter Builder
+    public MyClass(int  egerInitial, string stringInitial)
+        {
+            miInteger = egerInitial;
+            myString = stringInitial;
+        }
+
+        //Method That Displays Information
+    public void ShowInformation()
+        {
+            Console.WriteLine($"Integer: {miInteger}, String: {myString}");
+        }
+
+       // Destroyer(Finisher)
+    ~MyClass()
+        {
+            Console.WriteLine("Destroyed Object.");
+        }
+        public static void MethodStatic()
+        {
+            Console.WriteLine("This is a static method.");
+        }
+      //  Method with arguments
+        public void MethodWithArguments(int number, string text)
+        {
+            Console.WriteLine($"Number: {number}, Text: {text}");
+        }
+       // Parameter Pass-Through Method by Reference
+        public void MethodWithStepByReference(ref int value)
+        {
+            value *= 2;
+        }
+
+        //Parameter Pass-by-Value Method
+        public void MethodWithStepByValue(int value)
+        {
+            value += 10;
+        }
+       // Parameter Pass-Through Method Per Output
+    public void MethodWithStepByOutput(out string result)
+        {
+            result = "Hello from the method!";
+        }
+
+    }
+}
---
+++ MyClass/Program.cs
@@ -0,0 +1,25 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+
+        MyClass.MyClass interger = new(42, "Hello");
+
+        //Calling Methods
+        interger.ShowInformation();
+
+        MyClass.MyClass.MethodStatic();
+
+        int value = 5;
+        interger.MethodWithStepByReference(ref value);
+        Console.WriteLine($"Value after method: {value}");
+
+        interger.MethodWithStepByValue(value);
+        Console.WriteLine($"Value after method: {value}");
+
+        string message;
+        interger.MethodWithStepByOutput(out message);
+        Console.WriteLine(message);
+    }
+}
